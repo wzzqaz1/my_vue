e795cb904f13a65db57d9888a1abf1c9ea57af94
root <root@localhost.localdomain>
2024-09-13 15:10:23 +0800

    vue测试

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h3>老陈NB</h3>
+        <h3>2402NB^2</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+};
+</script>
+
+<style scoped lang="less">
+
+</style>
